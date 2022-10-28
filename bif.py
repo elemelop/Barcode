f414bf06811bf6c4d977b9b5f78369364a4e94ec
addy2354 <93484102+addy2354@users.noreply.github.com>
2022-10-28 22:05:40 +0530

    Update bif.py

--- bif.py
+++ bif.py
@@ -1,7 +1,6 @@
-#hello_world, Danish Abdullah here
-#i hope you all are doing well
+#abdullahpvt09
 #this is the source code of simple Barcode Importing library
 import qrcode
 
-generate_image = qrcode.make("https://instagram.com/oldstoner18?utm_medium=copy_link")
+generate_image = qrcode.make("https://www.instagram.com/abdullahpvt09/")
 generate_image.save('image.png')
