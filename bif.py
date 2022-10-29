4c1326be9599c3d0ddb0f03bd1b9149087f4e24c
Miran Hamid <miranhamid2002@gmail.com>
2022-10-29 14:31:45 +0530

    Updated the source code

--- bif.py
+++ bif.py
@@ -9,3 +9,6 @@ import qrcode
 
 generate_image = qrcode.make("https://instagram.com/oldstoner18?utm_medium=copy_link")
 generate_image.save('image.png')
+
+
+#this is an update for no reason at all but hey now I am an open source contributor lol ;) 
